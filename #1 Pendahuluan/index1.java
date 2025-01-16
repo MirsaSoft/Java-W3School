e73208991e88e2128b41e7478eca5f4dfb2f03ac
Mirsa Alif Putra <mirsaSoftBoy@gmail.com>
2025-01-16 12:46:41 +0700

    Create index1.java

---
+++ #1 Pendahuluan/index1.java
@@ -0,0 +1,14 @@
+public class index1 {
+    public static void main(String[] args){
+        //This is a comment
+        System.out.println("================");
+        System.out.println("TOKO SUMBER PADI");
+        System.out.println("================");
+        System.out.println("Tanggal :");
+        System.out.println("Harga :");
+        System.out.println("Jumlah : ");
+        System.out.println("================");
+        System.out.println("By : MIRSA");
+    } //This is a comment
+} /*This is a comment
+This is a comment */
